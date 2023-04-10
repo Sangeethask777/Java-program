b674463e008e0593919172e36b0c9e61d0668154
sange <sange@Sangeetha>
2023-04-10 10:40:10 +0530

    First Commit

---
+++ JavaPrograms/src/sampleprogram/Hello.java
@@ -0,0 +1,17 @@
+package sampleprogram;
+
+public class Hello {
+	public void input()
+	{
+		System.out.println("hello world");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Hello obj1=new Hello();
+
+obj1.input();
+
+	}
+
+}
